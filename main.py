39350b68f983335896a2007fe312017cfa028eff
Samantha Wu <samiekoo@gmail.com>
2019-12-18 17:45:45 -0500

    project

--- main.py
+++ main.py
@@ -7,26 +7,32 @@ input_path = sys.argv[1]
 output_path = sys.argv[2]
 
 img = Image.open(input_path)
-width, height = img.size
+width, height = img.size()
 
 # Create a new, all-white image that's the same size as the original
 new_img = Image.new("RGB", (width, height), "white")
 
 # TODO: Replace this with your own filter!
-# Median pixel filter, taken from https://note.nkmk.me/en/python-opencv-pillow-image-size
-members = [(0, 0)] * 9
-for i in range(1, width - 1):
-    for j in range(1, height - 1):
-        members[0] = img.getpixel((i - 1, j - 1))
-        members[1] = img.getpixel((i - 1, j))
-        members[2] = img.getpixel((i - 1, j + 1))
-        members[3] = img.getpixel((i, j - 1))
-        members[4] = img.getpixel((i, j))
-        members[5] = img.getpixel((i, j + 1))
-        members[6] = img.getpixel((i + 1, j - 1))
-        members[7] = img.getpixel((i + 1, j))
-        members[8] = img.getpixel((i + 1, j + 1))
-        members.sort()
-        new_img.putpixel((i, j), (members[4]))
+
+for x in width:
+    for y in height:
+        r, g, b = img.getpixel((x, y))
+        new_color = (r, g, b)
+        for z in new_color:
+            if r < 150:
+                r = 199
+            else:
+                r = 230
+            if g > 100:
+                g = 80
+            else:
+                g = 90
+            if b < 100:
+                b = 106
+            else:
+                b = 106
+        new_img.putpixel((x, y), (r, g, b))
+
+# i was attempting to tint the input image red, but i think i made an error somewhere.
 
 new_img.save(output_path, "JPEG")
